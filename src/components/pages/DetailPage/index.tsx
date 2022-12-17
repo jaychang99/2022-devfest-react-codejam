4d1c0d653abfff10907ba337a3855eb8cb618fdd
jaychang99 <jaychang99@gmail.com>
2022-12-17 11:51:02 +0900

    feat: DetailPage 에서 data fetching 연결 완료

--- src/components/pages/DetailPage/index.tsx
+++ src/components/pages/DetailPage/index.tsx
@@ -1,8 +1,12 @@
 import { AppScreen } from '@stackflow/plugin-basic-ui';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DetailPageBackButton } from 'src/components/common/Stackflow';
 import { useFlow } from 'src/utils/stackflow';
 import styled from 'styled-components';
+import { UserInterface } from 'src/schemas/Product';
+import { ProductInterface } from '../../../schemas/Product';
+import { getUserData } from 'src/services/user';
+import { getProductData } from 'src/services/product';
 
 type DetailParams = {
   params: {
@@ -13,6 +17,22 @@ type DetailParams = {
 const DetailPage: React.FC<DetailParams> = ({ params: { id } }) => {
   // stackflow
   const { pop } = useFlow();
+
+  const [product, setProduct] = useState<ProductInterface>();
+  const [user, setUser] = useState<UserInterface>();
+
+  const loadData = async () => {
+    const { data } = await getProductData(Number(id));
+
+    setProduct(data);
+    const { data: userData } = await getUserData(data.userID);
+
+    setUser(userData);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <AppScreen
